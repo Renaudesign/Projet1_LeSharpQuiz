de51c5c0e1439f881801bdf2cc57a6c2982d866a
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-22 10:26:26 +0100

    début idf

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projet1_Quizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            Console.WriteLine("Le Quizzzzzz");
+            Console.WriteLine("Entrez votre nom : ");
+            string snom = Console.ReadLine();
+            Console.WriteLine("Entrez votre prénom : ");
+            string sprenom = Console.ReadLine();
+            Console.WriteLine($"Bonjour {snom} {sprenom}, Bienvenue à notre quiz, êtes-vous prêt à améliorer vos connaissance en C# ? ");
+            Console.WriteLine("Taper entrée pour démarrer le test");
+            Console.ReadKey();
+            
+        }
+    }
+}
