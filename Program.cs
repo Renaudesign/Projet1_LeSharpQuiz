3bb64d6306e94538a2c0defac026debd1a58ec36
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-22 14:51:05 +0100

    IDF

--- Program.cs
+++ Program.cs
@@ -9,14 +9,22 @@ namespace Projet1_Quizz
            
             
             Console.WriteLine("Le Quizzzzzz");
+            Console.WriteLine("Entre votre genre");
+            string genre = Console.ReadLine();
+            
             Console.WriteLine("Entrez votre nom : ");
-            string snom = Console.ReadLine();
+            string nom = Console.ReadLine();
             Console.WriteLine("Entrez votre prénom : ");
-            string sprenom = Console.ReadLine();
-            Console.WriteLine($"Bonjour {snom} {sprenom}, Bienvenue à notre quiz, êtes-vous prêt à améliorer vos connaissance en C# ? ");
+            string prenom = Console.ReadLine();
+            Console.WriteLine($"Bonjour {genre} {nom} {prenom}, Bienvenue dans le quizz C#. \n 10 questions vous seront posées pour vérfier vos connaissances." +
+                $"\n Pour chaque question vous seront proposées 4 réponses." +
+                $"\n Vous pouvez séléctionner une réponse en tapant la lettre associée <A>, <B>, <C> ou <D> puis valider avec <Entrée>\nBon quizz!");
             Console.WriteLine("Taper entrée pour démarrer le test");
             Console.ReadKey();
-            
+
+
+
+           
         }
     }
 }
