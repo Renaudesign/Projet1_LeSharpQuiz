7ad705b413e1f9f1cdbafdbc792c61c445326935
renaudesign <renaud.sourmail@gmail.com>
2021-02-22 09:50:42 +0100

    le quizzzz

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Projet1_Quizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            Console.WriteLine("Le Quizzzzzz");
+        }
+    }
+}
