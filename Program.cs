78ab3fc53147a9ae4cf61b2c8a49266f147cdfd8
benoit.clement <benoit.cle@gmail.com>
2021-02-23 11:32:42 +0100

    working logic for display tests

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 
 namespace quizzGame
 {
@@ -11,7 +11,8 @@ namespace quizzGame
             // Each inner array corresponds to a step in survey with 5 strings corresponding to :
             // 1. question 2. the answer 3. 4. & 5. other choices
 
-            string[][] survey = new string[3][];
+
+            string[][] survey = new string[10][];
             survey[0] = new string[] {
                 @"Quel est le lien entre la plateforme .NET et C# ?",
                 @"Les applications .NET peuvent être écrit en C#",
@@ -22,20 +23,79 @@ namespace quizzGame
             survey[1] = new string[]
             {
                 @"En C, quelle est la séquence d'échappement correcte pour mettre une nouvelle ligne dans une chaîne de caractère « string » ?",
+                @"\n",
                 @"\br",
                 @"/n",
-                @"/br",
-                @"\n"
+                @"/br"
+
             };
             survey[2] = new string[]
             {
                 @"Pourquoi les « truth tables » sont-elles utiles aux programmeurs ?",
-                @"Une « truth table » répertorie tous les opérateurs booléens utilisés dans la programmation C #.",
-                @"Les « truth tables » visualisent la sortie d'un opérateur booléen avec toutes les combinaisons possibles d'entrées.",
-                @"Une table de vérité est un type de données en C # qui contient de nombreuses valeurs booléennes.",
+                @"Les « truth tables » visualisent la sortie d'un opérateur booléen avec toutes les combinaisons possibles d'entrées",
+                @"Une « truth table » répertorie tous les opérateurs booléens utilisés dans la programmation C #",
+                @"Une table de vérité est un type de données en C # qui contient de nombreuses valeurs booléennes",
                 @"Aucune de ces 3 suggestions"
             };
+            survey[3] = new string[]
+            {
+                @"Lequel n'est pas un membre du groupe ?",
+                @"Object",
+                @"Constructor",
+                @"Property",
+                @"Method"
 
+            };
+            survey[4] = new string[]
+            {
+                @"Qu'est-ce que LINQ ?",
+                @"Les fonctionnalités LINQ peuvent être utilisées dans un programme C# en important le namespace System.Linq",
+                @"La liste et les types de dictionnaires",
+                @"Les méthodes Select() et Where()",
+                @"Une syntaxe de requéte unique"
+
+            };
+            survey[5] = new string[]
+            {
+                @"Quel est le nom d'une boucle qui ne se termine jamais ?",
+                @"Une boucle infinie",
+                @"Une boucle récurrente",
+                @"Une boucle récursive",
+                @"Une boucle sans terminaison"
+            };
+            survey[6] = new string[]
+            {
+                @"Quelle ligne de code écrit correctement ""Hello World"" sur la console ?",
+                @"Console.WriteLine(""Hello World"");",
+                @"Console(""Hello World"");",
+                @"WriteLine(""Hello World"");",
+                @"Console.WriteLine(‘Hello World’);"
+
+            };
+            survey[7] = new string[]
+            {
+                @"En C #, comment s'appellent ces lignes de code ? /*Nothing to see here yet */",
+                @"Commentaire",
+                @"Plateforme .NET",
+                @"Commande",
+                @"Sortie"
+            };
+            survey[8] = new string[]
+            {
+                @"Que fait arr [4], en supposant que arr est un tableau ?",
+                @"Accède au 5ème élément du tableau",
+                @"Redimensionne le tableau afin qu'il puisse stocker 4 éléments",
+                @"Accède au 4ème élément du tableau",
+                @"Supprimez l'élément de tableau à l'index 4"
+            };
+            survey[9] = new string[]
+            {
+                @"Quelle ligne de commande est utilisée pour lire une entrée(input) de l’utilisateur en C# ?",
+                @"Console.ReadLine();",
+                @"Console.writeLine();",
+                @"Console.WriteLine();",
+                @"Console.readLine();"
+            };
 
             int[] userAnswers = new int[survey.Length];
             int userScore = 0;
@@ -63,7 +123,7 @@ namespace quizzGame
                 {
                     // bool argument of ReadKey() is for hiding user key stroke on console
                     keyPress = Console.ReadKey(true);
-                    Console.WriteLine($"Vous avez appuyé sur : < {keyPress.Key.ToString()} >");
+                    // Console.WriteLine($"Vous avez appuyé sur : < {keyPress.Key.ToString()} >");
                     if (keyPress.Key == ConsoleKey.A) userChoice = 1;
                     else if (keyPress.Key == ConsoleKey.B) userChoice = 2;
                     else if (keyPress.Key == ConsoleKey.C) userChoice = 3;
@@ -79,9 +139,21 @@ namespace quizzGame
 
             for (int i = 0; i < survey.Length; i++)
             {
+                string question = survey[i][0];
+                string goodAnswer = survey[i][1];
+                string[] choices = new string[] {
+                survey[i][1],
+                survey[i][2],
+                survey[i][3]
+                };
+                Random random = new Random();
+                
                 displayQuestion(survey[i]);
                 userAnswers[i] = getAnswer();
+                if (userAnswers[i] == 1) userScore++;
             }
+
+            Console.WriteLine($" Vous obtenez le score de {userScore} points sur {survey.Length}");
 
         }
     }
