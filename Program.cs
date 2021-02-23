af9a2074a17e8c94717198c1b6d2429208d87816
benoitclement <benoit.cle@gmail.com>
2021-02-23 01:49:49 +0100

    first draft for displayQuestion() and getAnswer() inside main loop. loop not working properly yet

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace quizzGame
             // Each inner array corresponds to a step in survey with 5 strings corresponding to :
             // 1. question 2. the answer 3. 4. & 5. other choices
 
-            string[][] survey = new string[10][];
+            string[][] survey = new string[3][];
             survey[0] = new string[] {
                 @"Quel est le lien entre la plateforme .NET et C# ?",
                 @"Les applications .NET peuvent être écrit en C#",
@@ -27,21 +27,61 @@ namespace quizzGame
                 @"/br",
                 @"\n"
             };
+            survey[2] = new string[]
+            {
+                @"Pourquoi les « truth tables » sont-elles utiles aux programmeurs ?",
+                @"Une « truth table » répertorie tous les opérateurs booléens utilisés dans la programmation C #.",
+                @"Les « truth tables » visualisent la sortie d'un opérateur booléen avec toutes les combinaisons possibles d'entrées.",
+                @"Une table de vérité est un type de données en C # qui contient de nombreuses valeurs booléennes.",
+                @"Aucune de ces 3 suggestions"
+            };
 
+
+            int[] userAnswers = new int[survey.Length];
             int userScore = 0;
 
-            Console.WriteLine("Bienvenue dans le quizz c#. 10 questions vous seront posées pour vérfier vos connaissances. Pour chaque question vous seront proposées 4 réponses. Vous pouvez séléctionner une réponse en tapant la lettre associée <A>, <B>, <C> ou <D> puis valider avec <Entrée> et passer à la question suivante\nBon quizz!");
+            Console.WriteLine("Bienvenue dans le quizz C#. 10 questions vous seront posées pour vérfier vos connaissances. Pour chaque question vous seront proposées 4 réponses. Vous pouvez séléctionner une réponse en tapant la lettre associée <A>, <B>, <C> ou <D> puis valider avec <Entrée> et passer à la question suivante\nBon quizz!\n");
 
-
-            foreach (string[] block in survey)
+            void displayQuestion(string[] questionAnswers)
             {
-                foreach (string element in block)
+                char incLetter = 'A';
+                Console.WriteLine("\n" + questionAnswers[0]);
+                    Console.WriteLine("------------------------");
+                for (int i = 1; i < questionAnswers.Length; i++) {
+                    Console.WriteLine($"{incLetter} - {questionAnswers[i]}");
+                    incLetter++;
+                    } 
+            }
+            int getAnswer()
+            {
+                int userChoice = 0;
+                bool exit = false;
+                ConsoleKeyInfo keyPress;
+
+                Console.WriteLine("Appuyez sur les <A> <B> <C> <D> puis <Entrée> pour valider");
+                while (!exit)
                 {
-                    Console.WriteLine(element);
+                    // bool argument of ReadKey() is for hiding user key stroke on console
+                    keyPress = Console.ReadKey(true);
+                    Console.WriteLine($"Vous avez appuyé sur : < {keyPress.Key.ToString()} >");
                 }
+
+                return userChoice;
             }
 
 
+            foreach (string[] question in survey)
+            {
+                displayQuestion(question);
+                
+            }
+
+            for (int i = 0; i < survey.Length; i++)
+            {
+                displayQuestion(survey[i]);
+                userAnswers[i] = getAnswer();
+            }
+            
 
 
 
