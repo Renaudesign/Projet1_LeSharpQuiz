d254c604d343690844332523ac18500729e15e8f
renaudesign <renaud.sourmail@gmail.com>
2021-02-23 11:05:19 +0100

    update du tableau

--- Program.cs
+++ Program.cs
@@ -19,29 +19,94 @@ namespace Projet1_Quizz
             Console.WriteLine("Taper entrée pour démarrer le test");
             Console.ReadKey();
 
-            Dictionary<string, string> question = new Dictionary<string, string>();
-            question.Add("Quel est le lien entre la plateforme .NET et C# ?", "Les applications .NET peuvent être écrit en C#");
-            question.Add("En C, quelle est la séquence d'échappement correcte pour mettre une nouvelle ligne dans une chaîne de caractère « string » ?", " \n ");
-            question.Add("Pourquoi les « truth tables » sont-elles utiles aux programmeurs ?", "Les « truth tables » visualisent la sortie d'un opérateur booléen avec toutes les combinaisons possibles d'entrées.");
-            question.Add("Lequel n'est pas un membre du groupe ?", "Object");
-            question.Add("Qu'est-ce que LINQ ?", "Les fonctionnalités LINQ peuvent être utilisées dans un programme C # en important le namespace System.Linq ");
-            question.Add("Quel est le nom d'une boucle qui ne se termine jamais ?", "Une boucle infinie. ");
-            //question.Add("Quelle ligne de code écrit correctement Hello World sur la console ?", "Console.WriteLine("Hello World"); ");
-            question.Add("En C #, comment s'appellent ces lignes de code ? /*Nothing to see here yet */ ", "Commentaire ");
-            question.Add("Que fait arr[4], en supposant que arr est un tableau ?", "Accède au 5ème élément du tableau. ");
-            question.Add("Quelle ligne de commande est utilisée pour lire une entrée(input) de l’utilisateur en C# ? ", "b.	Console.ReadLine() ;");
+            string[][] blocks = new string[10][];
+            blocks[0] = new string[] {
+                @"Quel est le lien entre la plateforme .NET et C# ?",
+                @"Les applications .NET peuvent être écrit en C#",
+                @".NET fait partie de C#",
+                @".NET est identique à C#",
+                @".NET n'est pas lié à C#"
+            };
+            blocks[1] = new string[]
+            {
+                @"En C, quelle est la séquence d'échappement correcte pour mettre une nouvelle ligne dans une chaîne de caractère « string » ?",
+                @"\n",
+                @"\br",
+                @"/n",
+                @"/br"
 
-            string enterQuestionnaire;
-            Console.WriteLine($" {question}Donnez la bonne réponse");
-            enterQuestionnaire = Console.ReadLine().ToUpper();
-            if (question.ContainsKey(enterQuestionnaire))
+            };
+            blocks[2] = new string[]
             {
-                Console.WriteLine(question[enterQuestionnaire]);
-            }
-            else
+                @"Pourquoi les « truth tables » sont-elles utiles aux programmeurs ?",
+                @"Les « truth tables » visualisent la sortie d'un opérateur booléen avec toutes les combinaisons possibles d'entrées",
+                @"Une « truth table » répertorie tous les opérateurs booléens utilisés dans la programmation C #",
+                @"Une table de vérité est un type de données en C # qui contient de nombreuses valeurs booléennes",
+                @"Aucune de ces 3 suggestions"
+            };
+            blocks[3] = new string[]
             {
-                Console.WriteLine("Entrer une valeur correcte");
-            }
+                @"Lequel n'est pas un membre du groupe ?",
+                @"Object",
+                @"Constructor",
+                @"Property",
+                @"Method"
+
+            };
+            blocks[4] = new string[]
+            {
+                @"Qu'est-ce que LINQ ?",
+                @"Les fonctionnalités LINQ peuvent être utilisées dans un programme C# en important le namespace System.Linq",
+                @"La liste et les types de dictionnaires",
+                @"Les méthodes Select() et Where()",
+                @"Une syntaxe de requéte unique"
+
+            };
+            blocks[5] = new string[]
+            {
+                @"Quel est le nom d'une boucle qui ne se termine jamais ?",
+                @"Une boucle infinie",
+                @"Une boucle récurrente",
+                @"Une boucle récursive",
+                @"Une boucle sans terminaison"
+            };
+            blocks[6] = new string[]
+            {
+                @"Quelle ligne de code écrit correctement ""Hello World"" sur la console ?",
+                @"Console.WriteLine(""Hello World"");",
+                @"Console(""Hello World"");",
+                @"WriteLine(""Hello World"");",
+                @"Console.WriteLine(‘Hello World’);"
+
+            };
+            blocks[7] = new string[]
+            {
+                @"En C #, comment s'appellent ces lignes de code ? /*Nothing to see here yet */",
+                @"Commentaire",
+                @"Plateforme .NET",
+                @"Commande",
+                @"Sortie"
+            };
+            blocks[8] = new string[]
+            {
+                @"Que fait arr [4], en supposant que arr est un tableau ?",
+                @"Accède au 5ème élément du tableau",
+                @"Redimensionne le tableau afin qu'il puisse stocker 4 éléments",
+                @"Accède au 4ème élément du tableau",
+                @"Supprimez l'élément de tableau à l'index 4"
+            };
+            blocks[9] = new string[]
+            {
+                @"Quelle ligne de commande est utilisée pour lire une entrée(input) de l’utilisateur en C# ?",
+                @"Console.ReadLine();",
+                @"Console.writeLine();",
+                @"Console.WriteLine();",
+                @"Console.readLine();"
+            };
+
+            // A la fin du quiz, vous devrez afficher un récapitulatif en donnant le score du candidat et les bonnes réponses.
+            ($"Bravo {nom} {prenom}, votre score est de {} voici les bonnes réponses au questionnaire {}");
+
         }
     }
 }
