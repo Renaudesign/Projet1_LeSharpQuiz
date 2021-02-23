4219340daee7d9852a23fd50c9ab4f4d4651e05c
renaudesign <renaud.sourmail@gmail.com>
2021-02-23 11:12:34 +0100

    blocks to survey

--- Program.cs
+++ Program.cs
@@ -19,15 +19,15 @@ namespace Projet1_Quizz
             Console.WriteLine("Taper entrée pour démarrer le test");
             Console.ReadKey();
 
-            string[][] blocks = new string[10][];
-            blocks[0] = new string[] {
+            string[][] survey = new string[10][];
+            survey[0] = new string[] {
                 @"Quel est le lien entre la plateforme .NET et C# ?",
                 @"Les applications .NET peuvent être écrit en C#",
                 @".NET fait partie de C#",
                 @".NET est identique à C#",
                 @".NET n'est pas lié à C#"
             };
-            blocks[1] = new string[]
+            survey[1] = new string[]
             {
                 @"En C, quelle est la séquence d'échappement correcte pour mettre une nouvelle ligne dans une chaîne de caractère « string » ?",
                 @"\n",
@@ -36,7 +36,7 @@ namespace Projet1_Quizz
                 @"/br"
 
             };
-            blocks[2] = new string[]
+            survey[2] = new string[]
             {
                 @"Pourquoi les « truth tables » sont-elles utiles aux programmeurs ?",
                 @"Les « truth tables » visualisent la sortie d'un opérateur booléen avec toutes les combinaisons possibles d'entrées",
@@ -44,7 +44,7 @@ namespace Projet1_Quizz
                 @"Une table de vérité est un type de données en C # qui contient de nombreuses valeurs booléennes",
                 @"Aucune de ces 3 suggestions"
             };
-            blocks[3] = new string[]
+            survey[3] = new string[]
             {
                 @"Lequel n'est pas un membre du groupe ?",
                 @"Object",
@@ -53,7 +53,7 @@ namespace Projet1_Quizz
                 @"Method"
 
             };
-            blocks[4] = new string[]
+            survey[4] = new string[]
             {
                 @"Qu'est-ce que LINQ ?",
                 @"Les fonctionnalités LINQ peuvent être utilisées dans un programme C# en important le namespace System.Linq",
@@ -62,7 +62,7 @@ namespace Projet1_Quizz
                 @"Une syntaxe de requéte unique"
 
             };
-            blocks[5] = new string[]
+            survey[5] = new string[]
             {
                 @"Quel est le nom d'une boucle qui ne se termine jamais ?",
                 @"Une boucle infinie",
@@ -70,7 +70,7 @@ namespace Projet1_Quizz
                 @"Une boucle récursive",
                 @"Une boucle sans terminaison"
             };
-            blocks[6] = new string[]
+            survey[6] = new string[]
             {
                 @"Quelle ligne de code écrit correctement ""Hello World"" sur la console ?",
                 @"Console.WriteLine(""Hello World"");",
@@ -79,7 +79,7 @@ namespace Projet1_Quizz
                 @"Console.WriteLine(‘Hello World’);"
 
             };
-            blocks[7] = new string[]
+            survey[7] = new string[]
             {
                 @"En C #, comment s'appellent ces lignes de code ? /*Nothing to see here yet */",
                 @"Commentaire",
@@ -87,7 +87,7 @@ namespace Projet1_Quizz
                 @"Commande",
                 @"Sortie"
             };
-            blocks[8] = new string[]
+            survey[8] = new string[]
             {
                 @"Que fait arr [4], en supposant que arr est un tableau ?",
                 @"Accède au 5ème élément du tableau",
@@ -95,7 +95,7 @@ namespace Projet1_Quizz
                 @"Accède au 4ème élément du tableau",
                 @"Supprimez l'élément de tableau à l'index 4"
             };
-            blocks[9] = new string[]
+            survey[9] = new string[]
             {
                 @"Quelle ligne de commande est utilisée pour lire une entrée(input) de l’utilisateur en C# ?",
                 @"Console.ReadLine();",
